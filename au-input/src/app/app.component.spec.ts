5c1cce918e499ebcf36c342249f96d55b227363b
Julian <jlmurraygarcia@gmail.com>
2023-02-07 12:16:43 -0800

    S3L27: Began creating tests for email input; Currently passing 4 specs, failing 1

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {AuFaInputComponent} from "./lib/au-fa-input/au-fa-input.component";
 import {InputRefDirective} from "./lib/common/input-ref.directive";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 // jasmine is our test framework
 
 describe('AppComponent', () => {
+
+  let component: AppComponent,
+      fixture: ComponentFixture<AppComponent>,
+      el: DebugElement;
 
   // before each test
   beforeEach(async () => {
@@ -17,13 +23,26 @@ describe('AppComponent', () => {
     }).compileComponents(); // tests only occur after components are compiled
   });
 
+  // before each test
+  beforeEach(() => {
+    // create a component to test
+    fixture = TestBed.createComponent(AppComponent);
+    // access component param
+    component = fixture.componentInstance;
+    // defined debug element
+    el = fixture.debugElement;
+  });
+
   // each 'it' is a test
   it('should create the app', () => {
-    // create a component to test
-    const fixture = TestBed.createComponent(AppComponent);
-    // access component param
-    const app = fixture.componentInstance;
     // execute test
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('should create a font awesome email input', () => {
+    const emailField = el.query(By.css('#email-field'));
+
+    expect(emailField).toBeTruthy();
+    expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
+  })
 });
