627176173d613bc04eb766d9646df625ff35a549
Julian <jlmurraygarcia@gmail.com>
2023-02-07 11:55:56 -0800

    S3L24: Created a library module

--- au-input/src/app/app.module.ts
+++ au-input/src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
-import { InputRefDirective } from './lib/common/input-ref.directive';
+import {AuInputModule} from "./lib/au-input.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuFaInputComponent,
-    InputRefDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    AuInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
