986889c25912a43700aa43ca1d715d03bdfa2520
Julian <jlmurraygarcia@gmail.com>
2023-02-07 10:34:52 -0800

    S2L16: Added ContentChild decorator

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,17 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, Input} from '@angular/core';
 
 @Component({
   selector: 'app-au-fa-input',
   templateUrl: './au-fa-input.component.html',
   styleUrls: ['./au-fa-input.component.scss']
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements AfterContentInit {
 
-  @Input() icon?: string;
+  @Input()
+  icon?: string;
+
+  // embedded <input #input> html elements
+  @ContentChild('input')
+  input!: HTMLInputElement;
 
   constructor() {  }
 
-  ngOnInit() {  }
+  ngAfterContentInit() {
+    console.log('testing testing is input here:', this.input);
+  }
 
   get classes(): { [key: string]: boolean; } {
     let cssClasses: { [key: string]: boolean; } = { };
