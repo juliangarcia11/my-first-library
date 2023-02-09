63844e3451c9722b92413f23cc972ae6d41511fa
Julian <jlmurraygarcia@gmail.com>
2023-02-09 07:27:46 -0800

    S6L60: Added testing to see if modal is open on start up and after click; Added testing wrapper component to use AuOpenModalOnClickDirective;

--- au-modal/src/app/app.component.spec.ts
+++ au-modal/src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {DebugElement} from "@angular/core";
-import {AuInputModule} from "./au-input-module/au-input.module";
-import {AuTabPanelModule} from "./au-tab-panel-module/au-tab-panel.module";
+import {AuFaInputComponent} from "./au-input-module/au-fa-input/au-fa-input.component";
+import {AuMdInputComponent} from "./au-input-module/au-md-input/au-md-input.component";
+import {AuTabPanelComponent} from "./au-tab-panel-module/au-tab-panel/au-tab-panel.component";
+import {AuTabComponent} from "./au-tab-panel-module/au-tab/au-tab.component";
+import {AuModalOpenOnClickDirective} from "./au-modal-module/au-modal-open-on-click.directive";
+import {By} from "@angular/platform-browser";
 import {AuModalComponent} from "./au-modal-module/au-modal/au-modal.component";
 
 // jasmine is our test framework
@@ -11,14 +15,21 @@ describe('AppComponent', () => {
 
   let component: AppComponent,
       fixture: ComponentFixture<AppComponent>,
-      el: DebugElement;
+      el: DebugElement,
+      modal: DebugElement;
 
   // before each test
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         // add components to test here
-        AppComponent, AuInputModule, AuTabPanelModule, AuModalComponent
+        AppComponent,
+        AuFaInputComponent,
+        AuMdInputComponent,
+        AuTabPanelComponent,
+        AuTabComponent,
+        AuModalOpenOnClickDirective,
+        AuModalComponent
       ],
     }).compileComponents(); // tests only occur after components are compiled
   });
@@ -29,6 +40,7 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     el = fixture.debugElement;
+    modal = el.query(By.css('#testModal'));
 
     // run angular change detections
     fixture.detectChanges();
@@ -38,4 +50,17 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not add the modal to the page if the modal is closed', () => {
+    expect(modal).toBeFalsy();
+  });
+
+  it('should open the modal when the test button is clicked', () => {
+    fixture.debugElement.nativeElement.querySelector('#testButton').click();
+    fixture.detectChanges();
+
+    const openedModal = fixture.debugElement.nativeElement.querySelector('#testModal');
+    expect(openedModal).toBeTruthy();
+  })
+
 });
--- au-modal/src/app/au-modal-module/au-modal-open-on-click.directive.spec.ts
+++ au-modal/src/app/au-modal-module/au-modal-open-on-click.directive.spec.ts
@@ -1,8 +1,45 @@
 import { AuModalOpenOnClickDirective } from './au-modal-open-on-click.directive';
+import {AuModalComponent} from "./au-modal/au-modal.component";
+import {Component, DebugElement, ViewChild} from "@angular/core";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {By} from "@angular/platform-browser";
+
+@Component({
+  template: `
+    <app-au-modal *appAuModalOpenOnClick="testerButton"></app-au-modal>
+    <button #testerButton id="testerButton"></button>
+  `,
+})
+class WrapperComponent {
+  @ViewChild(AuModalComponent, { static: true }) appComponentRef: AuModalComponent | undefined;
+}
 
 describe('AuModalOpenOnClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AuModalOpenOnClickDirective();
-    expect(directive).toBeTruthy();
+  let directives: DebugElement[];
+  let fixture: ComponentFixture<WrapperComponent>;
+
+  beforeEach( async() => {
+    await TestBed.configureTestingModule({
+      declarations: [WrapperComponent, AuModalOpenOnClickDirective, AuModalComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+
+    // const triggerBtn = fixture.debugElement.query(By.css('#testerButton'));
+    // triggerBtn.nativeElement.click();
+
+    fixture.detectChanges();
+    directives = fixture.debugElement.queryAll(By.directive(AuModalOpenOnClickDirective));
+
+  })
+
+  it('should show the directive is truthy', () => {
+    const triggerBtn = fixture.debugElement.query(By.css('#testerButton'));
+    triggerBtn.nativeElement.click();
+    fixture.detectChanges();
+    console.log('what is a directive', directives);
+    expect(directives).toBeTruthy();
   });
 });
