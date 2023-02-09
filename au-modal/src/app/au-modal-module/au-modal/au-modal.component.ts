dc0b07019fe073cbe038a253c51f28390a3d06ee
Julian <jlmurraygarcia@gmail.com>
2023-02-09 05:16:36 -0800

    S6L56: Added customizable options for when the modal should hide (on esc or on click outside the modal)

--- au-modal/src/app/au-modal-module/au-modal/au-modal.component.ts
+++ au-modal/src/app/au-modal-module/au-modal/au-modal.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, TemplateRef} from '@angular/core';
+import {Component, HostListener, Input, OnInit, TemplateRef} from '@angular/core';
 import {AuModalService} from "../au-modal.service";
+import {EventManager} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-au-modal',
@@ -9,12 +10,29 @@ import {AuModalService} from "../au-modal.service";
 export class AuModalComponent {
 
   constructor(
-    private modalService: AuModalService
+    private modalService: AuModalService,
+    private eventManager: EventManager
   ) { }
 
   // hold a references to an injectable template
   @Input()
   body: TemplateRef<any> | undefined;
+
+  // allow references to customizable behavior flags
+  @Input()
+  hideOnEsc: boolean = true;
+
+  // allow references to customizable behavior flags
+  @Input()
+  hideOnClickOutside: boolean = true;
+
+  // listen for an event on the host,
+  // in this case: the upwards motion of the escape key when the user is focused anywhere on the window
+  @HostListener('window:keyup.esc', ['$event'])
+  keyEsc(event: KeyboardEvent): void {
+    if (this.hideOnEsc)
+      this.closeModal();
+  }
 
   /**
    * Request that the modal service communicate the closure request
@@ -30,5 +48,13 @@ export class AuModalComponent {
   cancelClick(event: MouseEvent) {
     event.preventDefault();
     event.stopPropagation();
+  }
+
+  /**
+   * Closes the modal on a click outside the modal, iif the hideOnClickOutside flag is set to true
+   */
+  onClickOutsideModal() {
+    if (this.hideOnClickOutside)
+      this.closeModal();
   }
 }
