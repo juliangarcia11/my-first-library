0786f16ae9926b88c924710efa92c675035a985c
Julian <jlmurraygarcia@gmail.com>
2023-02-09 09:45:39 -0800

    S7L64: Mask directive placeholder first implementation with the reduce function

--- au-mask/src/app/au-mask/au-mask.directive.ts
+++ au-mask/src/app/au-mask/au-mask.directive.ts
@@ -1,4 +1,5 @@
 import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {SPECIAL_CHARACTERS} from "./mask.utils";
 
 @Directive({
   selector: '[au-mask]'
@@ -17,7 +18,19 @@ export class AuMaskDirective implements OnInit {
   }
 
   ngOnInit(): void {
+    // set the initial input value as the page loads up
+    this.input.value = this.buildPlaceHolder();
 
   }
 
+  /**
+   * Using the mask param, build a placeholder string made from chars from the SPECIAL_CHARACTERS list and underscores
+   * @private
+   */
+  private buildPlaceHolder(): string {
+    const chars = this.mask.split('');
+    return chars.reduce((result, char) => {
+      return result += SPECIAL_CHARACTERS.includes(char) ? char : '_';
+    }, '');
+  }
 }
--- au-mask/src/app/au-mask/mask.utils.ts
+++ au-mask/src/app/au-mask/mask.utils.ts
@@ -1,11 +1,8 @@
-
-
-
 export const  TAB = 9,
-    LEFT_ARROW =	37,
-    RIGHT_ARROW = 39,
-    BACKSPACE = 8,
-    DELETE = 46;
+              LEFT_ARROW =	37,
+              RIGHT_ARROW = 39,
+              BACKSPACE = 8,
+              DELETE = 46;
 
 export const SPECIAL_CHARACTERS = [" ", "/", "(", ")", "+", "\/", "-"];
 
