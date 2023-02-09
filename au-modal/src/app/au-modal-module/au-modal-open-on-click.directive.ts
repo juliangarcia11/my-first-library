682cc892f5680e04357f3854f93c696a91766f44
Julian <jlmurraygarcia@gmail.com>
2023-02-09 06:21:36 -0800

    S6L59: Added event listener removal to prevent memory leaks in the AuModalOpenOnClickDirective

--- au-modal/src/app/au-modal-module/au-modal-open-on-click.directive.ts
+++ au-modal/src/app/au-modal-module/au-modal-open-on-click.directive.ts
@@ -1,14 +1,16 @@
-import {ContentChild, Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+import {ContentChild, Directive, Input, OnDestroy, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 import {AuModalComponent} from "./au-modal/au-modal.component";
 import {AuModalService} from "./au-modal.service";
 
 @Directive({
   selector: '[appAuModalOpenOnClick]'
 })
-export class AuModalOpenOnClickDirective implements OnInit {
+export class AuModalOpenOnClickDirective implements OnInit, OnDestroy {
 
   @ContentChild(AuModalComponent)
   modal: AuModalComponent | undefined;
+
+  elements: HTMLElement[] = [];
 
   constructor(
     private templateRef: TemplateRef<any>,
@@ -16,9 +18,20 @@ export class AuModalOpenOnClickDirective implements OnInit {
     private modalService: AuModalService
   ) { }
 
+  /**
+   * Actions to perform as soon as initialization of the directive is complete
+   */
   ngOnInit() {
     // listen to the modalService's close$ observable, when it fires, clear the view container this directive is attached to
     this.modalService.close$.subscribe(() => this.viewContainer.clear());
+  }
+
+  /**
+   * Actions to perform on destruction of the directive from the dom
+   */
+  ngOnDestroy() {
+    // clear any event listeners to prevent memory leaks
+    this.elements.forEach(el => el.removeEventListener('click', this.clickHandler));
   }
 
   /**
@@ -28,24 +41,29 @@ export class AuModalOpenOnClickDirective implements OnInit {
    */
   @Input()
   set appAuModalOpenOnClick(els: HTMLElement | HTMLElement[]) {
-    let elements: HTMLElement[]; // placeholder
-
     // verify els are an array of HTMLElements
     if (Array.isArray(els)) {
-      elements = els;
+      this.elements = els;
     } else {
-      elements = [els];
+      this.elements = [els];
     }
 
     // for each el in the array of elements
-    elements.forEach(el => {
-      // add a click event listener
-      el.addEventListener('click', () => {
-        // clear this view's container
-        this.viewContainer.clear();
-        // re-embed the view container this directive is attached to
-        this.viewContainer.createEmbeddedView(this.templateRef);
-      });
+    this.elements.forEach(el => {
+      // attach a click handler function to an event listener on the el
+      el.addEventListener('click', this.clickHandler);
     });
   }
+
+  /**
+   * Odd syntax ().bind(this) -- binds the function to the directive's 'this' scope
+   *
+   * Clear the view container and embed the view created from the provided template
+   */
+  clickHandler = (() => {
+    // clear this view's container
+    this.viewContainer.clear();
+    // re-embed the view container this directive is attached to
+    this.viewContainer.createEmbeddedView(this.templateRef);
+  }).bind(this);
 }
